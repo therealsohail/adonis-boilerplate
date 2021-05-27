156fd0b54822d3d85889e2d893c68d63cf539fa7
waqar fomark <waqar.fomark@gmail.com>
2021-05-27 14:40:14 +0500

    changed response data to null instead of {}

--- app/Commands/ApiController.js
+++ app/Commands/ApiController.js
@@ -36,13 +36,13 @@ class ${this.args.name}Controller extends BaseController {
         let orderBy = ctx.request.input('orderBy', 'id')
         let sortBy = ctx.request.input('sortBy', 'desc')
         let res = await ${this.args.name}Repo.index(offset,limit,orderBy,sortBy)
-        return ctx.response.json(this.globalResponse(true, "Record fetched successfully!", res.toJSON()))
+        return ctx.response.json(this.globalResponse(true, "Record fetched successfully!", res))
     }
 
     //Show single record
     async show(ctx) {
         const res = await ${this.args.name}Repo.show(ctx.request.params.id)
-        return ctx.response.json(this.globalResponse(true, "Record fetched successfully!", res ? res.toJSON() : {}))
+        return ctx.response.json(this.globalResponse(true, "Record fetched successfully!", res))
     }
 
     //Save a record
@@ -57,7 +57,7 @@ class ${this.args.name}Controller extends BaseController {
         const input = ctx.request.only(${this.args.name}Repo.model.fillable)
         let res = await ${this.args.name}Repo.update(ctx.request.params.id, input)
         if (!res){
-            return ctx.response.json(this.globalResponse(false, "Record not found!",{}))
+            return ctx.response.json(this.globalResponse(false, "Record not found!"))
         }else{
             return ctx.response.json(this.globalResponse(true, "Record updated successfully!", res))
         }
@@ -67,9 +67,9 @@ class ${this.args.name}Controller extends BaseController {
     async destroy(ctx) {
         const res = await ${this.args.name}Repo.destroy(ctx.request.params.id)
         if(!res){
-            return ctx.response.json(this.globalResponse(false, "Record not found!", {}))
+            return ctx.response.json(this.globalResponse(false, "Record not found!"))
         }else{
-            return ctx.response.json(this.globalResponse(true, "Record deleted successfully!", {}))
+            return ctx.response.json(this.globalResponse(true, "Record deleted successfully!"))
         }
     }
 
